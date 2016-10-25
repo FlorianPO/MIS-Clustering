94e711fc124bfff01e3c320b8ed04f681e43286a
FlorianPO <florian.popek@gmail.com>
2016-10-25 14:28:31 +0200

    Added toString method to BFSMessage

--- mis-clustering/src/projects/kcluster/nodes/messages/BFSMessage.java
+++ mis-clustering/src/projects/kcluster/nodes/messages/BFSMessage.java
@@ -3,18 +3,28 @@ package projects.kcluster.nodes.messages;
 import sinalgo.nodes.messages.Message;
 
 public class BFSMessage extends Message {
+	private static int BFS_cpt = 0;
 
+	public int cpt;
 	public int distance;
 	public int ID;
 
 	public BFSMessage(int ID, int distance) {
 		this.ID = ID;
 		this.distance = distance;
+		cpt = BFS_cpt;
+		BFS_cpt++;
+		System.out.println("________" + cpt);
 	}
 
 	@Override
 	public Message clone() {
 		return this;
+	}
+
+	@Override
+	public String toString() {
+		return "ID_" + ID + ", d_" + distance + " (BFS " + cpt + ")";
 	}
 
 }
